acf76516a1a91eddc799de295f6c0ea1a975286b
David Sibley <david.sibley@digital.justice.gov.uk>
2022-04-29 16:22:43 +0100

    added all non-refreshable checks seen in logs

--- function/utilities.js
+++ function/utilities.js
@@ -57,7 +57,16 @@ const utilities = {
     'Hs4Ma3G123', // EC2 instances should not have a public IPv4 address
     'Hs4Ma3G124', // EC2 instances should use Instance Metadata Service Version 2
     'Hs4Ma3G125', // API Gateway should be associated with a WAF Web ACL
+    'Hs4Ma3G126', // DynamoDB Accelerator (DAX) clusters should be encrypted at rest
+    'Hs4Ma3G127', // API Gateway REST and WebSocket API execution logging should be enabled
+    'Hs4Ma3G128', // API Gateway REST API stages should be configured to use SSL certificates for backend authentication
+    'Hs4Ma3G129', // API Gateway REST API stages should have AWS X-Ray tracing enabled
+    'Hs4Ma3G130', // Lambda functions should use supported runtimes
+    'Hs4Ma3G131', // Lambda function policies should prohibit public access
+    'Hs4Ma3G132', // Database Migration Service replication instances should not be public
+    'Hs4Ma3G133', // IAM customer managed policies should not allow decryption actions on all KMS keys
     'Hs4Ma3G134', // Cannot refresh IAM principals should not have IAM inline policies that allow decryption actions on all KMS keys
+    'Hs4Ma3G135', // AWS KMS keys should not be deleted unintentionally
     'Hs4Ma3G136', // Cannot refresh Amazon SQS queues should be encrypted at rest
     'Hs4Ma3G137', // IAM policies should not allow full "*" administrative privileges
     'Hs4Ma3G138', // IAM users should not have IAM policies attached
@@ -75,12 +84,64 @@ const utilities = {
     'Hs4Ma3G150', // Elasticsearch domains should encrypt data sent between nodes
     'Hs4Ma3G151', // An RDS event notifications subscription should be configured for critical database parameter group events
     'Hs4Ma3G152', // An RDS event notifications subscription should be configured for critical database instance events
+    'Hs4Ma3G153', // RDS instances should not use a database engine default port
+    'Hs4Ma3G154', // An RDS event notifications subscription should be configured for critical database security group events
+    'Hs4Ma3G155', // EC2 instances should be managed by AWS Systems Manager
+    'Hs4Ma3G156', // EC2 instances managed by Systems Manager should have a patch compliance status of COMPLIANT after a patch installation
+    'Hs4Ma3G157', // EC2 instances managed by Systems Manager should have an association compliance status of COMPLIANT
+    'Hs4Ma3G158', // SSM documents should not be public
+    'Hs4Ma3G159', // Elastic File System should be configured to encrypt file data at-rest using AWS KMS
+    'Hs4Ma3G160', // IAM authentication should be configured for RDS instances
+    'Hs4Ma3G161', // IAM authentication should be configured for RDS clusters
+    'Hs4Ma3G162', // RDS automatic minor version upgrades should be enabled
+    'Hs4Ma3G163', // RDS DB clusters should be configured to copy tags to snapshots
+    'Hs4Ma3G164', // RDS DB instances should be configured to copy tags to snapshots
+    'Hs4Ma3G165', // RDS instances should be deployed in a VPC
+    'Hs4Ma3G166', // An RDS event notifications subscription should be configured for critical cluster events
+    'Hs4Ma3G167', // S3 buckets should have server-side encryption enabled
+    'Hs4Ma3G168', // S3 buckets should require requests to use Secure Socket Layer
+    'Hs4Ma3G169', // S3 permissions granted to other AWS accounts in bucket policies should be restricted
+    'Hs4Ma3G170', // S3 Block Public Access setting should be enabled
+    'Hs4Ma3G171', // S3 buckets should prohibit public read access
+    'Hs4Ma3G172', // S3 buckets should prohibit public write access
+    'Hs4Ma3G173', // S3 Block Public Access setting should be enabled at the bucket-level
     'Hs4Ma3G174', // CodeBuild GitHub or Bitbucket source repository URLs should use OAuth
+    'Hs4Ma3G175', // CodeBuild project environment variables should not contain clear text credentials
+    'Hs4Ma3G176', // ACM certificates should be renewed after a specified time period
     'Hs4Ma3G177', // Auto scaling groups associated with a load balancer should use load balancer health checks
     'Hs4Ma3G178', // Security groups should only allow unrestricted incoming traffic for authorized ports
     'Hs4Ma3G179', // SNS topics should be encrypted at-rest using AWS KMS
     'Hs4Ma3G180', // Amazon Elasticsearch Service domain error logging to CloudWatch Logs should be enabled
     'Hs4Ma3G181', // Classic Load Balancers with SSL/HTTPS listeners should use a certificate provided by AWS Certificate Manager'
+    'Hs4Ma3G182', // Classic Load Balancer listeners should be configured with HTTPS or TLS termination
+    'Hs4Ma3G183', // Application load balancer should be configured to drop http headers
+    'Hs4Ma3G184', // Application and Classic Load Balancers logging should be enabled
+    'Hs4Ma3G185', // IAM customer managed policies that you create should not allow wildcard actions for services
+    'Hs4Ma3G186', // AWS WAF Classic Global Web ACL logging should be enabled
+    'Hs4Ma3G187', // Connections to Amazon Elasticsearch Service domains should be encrypted using TLS 1.2
+    'Hs4Ma3G188', // GuardDuty should be enabled
+    'Hs4Ma3G189', // Enhanced monitoring should be configured for RDS DB instances
+    'Hs4Ma3G190', // RDS clusters should have deletion protection enabled
+    'Hs4Ma3G191', // RDS cluster snapshots and database snapshots should be encrypted at rest
+    'Hs4Ma3G192', // RDS DB Instances should prohibit public access, determined by the PubliclyAccessible configuration
+    'Hs4Ma3G193', // RDS DB instances should have encryption at-rest enabled
+    'Hs4Ma3G194', // RDS snapshot should be private
+    'Hs4Ma3G195', // CloudFront distributions should have origin access identity enabled
+    'Hs4Ma3G196', // AWS Config should be enabled
+    'Hs4Ma3G197', // Amazon Elasticsearch Service domains should have encryption at-rest enabled
+    'Hs4Ma3G198', // RDS DB instances should have deletion protection enabled
+    'Hs4Ma3G199', // Database logging should be enabled
+    'Hs4Ma3G200', // CloudFront distributions should have a default root object configured
+    'Hs4Ma3G201', // CloudFront distributions should have WAF enabled
+    'Hs4Ma3G202', // API Gateway REST API cache data should be encrypted at rest
+    'Hs4Ma3G203', // Amazon Elasticsearch Service domains should have audit logging enabled
+    'Hs4Ma3G204', // Security groups should not allow unrestricted access to ports with high risk
+    'Hs4Ma3G205', // Classic Load Balancers with HTTPS/SSL listeners should use a predefined security policy that has strong configuration
+    'Hs4Ma3G206', // Amazon EC2 should be configured to use VPC endpoints that are created for the Amazon EC2 service
+    'Hs4Ma3G207', // EC2 subnets should not automatically assign public IP addresses
+    'Hs4Ma3G208', // EC2 instances should not use multiple ENIs
+    'Hs4Ma3G209', // Unused Network Access Control Lists should be removed
+    'Hs4Ma3G210', // CloudFront distributions should have logging enabled
     'L4dfs2Q3C2', // AWS Lambda Functions with High Error Rates
     'L4dfs2Q3C3', // AWS Lambda Functions with Excessive Timeouts
     'L4dfs2Q4C5', // AWS Lambda Functions Using Deprecated Runtimes
@@ -92,7 +153,7 @@ const utilities = {
     'Wxdfp4B1L1', // AWS Well-Architected high risk issues for cost optimization
     'Wxdfp4B1L2', // AWS Well-Architected high risk issues for performance efficiency
     'Wxdfp4B1L3', // AWS Well-Architected high risk issues for security
-    'Wxdfp4B1L4', // AWS Well-Architected high risk issues for reliability
+    'Wxdfp4B1L4' // AWS Well-Architected high risk issues for reliability
   ],
   describeTrustedAdvisorChecks () {
     return support.describeTrustedAdvisorChecks({
