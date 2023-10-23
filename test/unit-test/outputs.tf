637147793c0b7f839c76d9f4022692dc2720aa5d
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-10-23 23:11:35 +0100

    ✨Add outputs to module under test

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -1,3 +1,15 @@
 output "lambda_function_name" {
   value = module.module_test.lambda_function_name
 }
+
+output "lambda_function_invoke_arn" {
+  value = module.module_test.lambda_function_invoke_arn
+}
+
+output "lambda_function_arn" {
+  value = module.module_test.lambda_function_arn
+}
+
+output "iam_role_arn" {
+  value = module.module_test.iam_role_arn
+}
