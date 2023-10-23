065c441d064405dc727759a8c1be9cf593c0a7e0
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-10-23 22:11:25 +0100

    ✨Add test for module outputs

--- test/module_test.go
+++ test/module_test.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-	"fmt"
 	awsSDK "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	testStructure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
+	"regexp"
 	"testing"
 )
 
-func TestLamdaFunctionRuns(t *testing.T) {
+func TestModuleOutputs(t *testing.T) {
 	t.Parallel()
 	uniqueId := random.UniqueId()
-	terraformDir := test_structure.CopyTerraformFolderToTemp(t, "..", "test/unit-test")
+	terraformDir := testStructure.CopyTerraformFolderToTemp(t, "..", "test/unit-test")
+	name := "testing-trusted-advisor-refresh-" + uniqueId
+	iamRoleName := "testing-AWSTrustedAdvisorRefresh-" + uniqueId
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: terraformDir,
+		Vars: map[string]interface{}{
+			"name":          name,
+			"iam_role_name": iamRoleName,
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	lambdaFunctionName := terraform.Output(t, terraformOptions, "lambda_function_name")
+	lambdaFunctionInvokeArn := terraform.Output(t, terraformOptions, "lambda_function_invoke_arn")
+	lambdaFunctionArn := terraform.Output(t, terraformOptions, "lambda_function_arn")
+
+	assert.Regexp(t, regexp.MustCompile(`^`+name), lambdaFunctionName)
+	assert.Regexp(t, regexp.MustCompile(`(arn:(aws[a-zA-Z-]*)?:lambda:)?([a-z]{2}(-gov)?-[a-z]+-\d{1}:)?(\d{12}:)?(function:)?([a-zA-Z0-9-_\.]+)(:(\$LATEST|`+name+`))?`), lambdaFunctionInvokeArn)
+	assert.Regexp(t, regexp.MustCompile(`(arn:(aws[a-zA-Z-]*)?:lambda:)?([a-z]{2}(-gov)?-[a-z]+-\d{1}:)?(\d{12}:)?(function:)?([a-zA-Z0-9-_\.]+)(:(\$LATEST|`+name+`))?`), lambdaFunctionArn)
+}
+
+func TestLamdaFunctionIsCreated(t *testing.T) {
+	t.Parallel()
+	uniqueId := random.UniqueId()
+	terraformDir := testStructure.CopyTerraformFolderToTemp(t, "..", "test/unit-test")
 	name := "testing-trusted-advisor-refresh-" + uniqueId
 	iamRoleName := "testing-AWSTrustedAdvisorRefresh-" + uniqueId
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -32,13 +58,9 @@ func TestLamdaFunctionRuns(t *testing.T) {
 	lambdaFunctionName := terraform.Output(t, terraformOptions, "lambda_function_name")
 
 	lambdaClient := aws.NewLambdaClient(t, "eu-west-2")
-	function, err := lambdaClient.GetFunction(&lambda.GetFunctionInput{
+	function, _ := lambdaClient.GetFunction(&lambda.GetFunctionInput{
 		FunctionName: &lambdaFunctionName,
 	})
-
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	assert.Equal(t, lambdaFunctionName, awsSDK.StringValue(function.Configuration.FunctionName))
 }
